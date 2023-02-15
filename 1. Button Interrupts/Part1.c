6d71db43c03c8e58b3edc8a3f635a0196819324b
Cippppy <98924381+Cippppy@users.noreply.github.com>
2023-02-15 12:50:16 -0500

    Set up CCS project, finished Part1.c

--- 1. Button Interrupts/Part1.c
+++ 1. Button Interrupts/Part1.c
@@ -2,7 +2,8 @@
  * Part1.c
  *
  *  Created on: Feb 11, 2023
- *      Author: Russell Trafford
+ *  Edited on: Feb 15, 2023
+ *      Author: Russell Trafford and Christian Cipolletta
  *
  *  This code is a template which will change the color of the LED being blinked using the interrupt routine.
  */
@@ -11,6 +12,8 @@
 #include <msp430.h>
 
 char LED_Color = 0x01;                       // Global Variable to determine which LED should be blinking
+char risingEdge = 0x00;                      // Global variable to determine if the interrupt should be looking for rising edge
+char fallingEdge = 0x01;                     // // Global variable to determine if the interrupt should be looking for falling edge
 
 void gpioInit();
 
@@ -35,10 +38,14 @@ int main(void)
 
     while(1)
     {
-        if (LED_Color)
+        if (LED_Color) {
             P1OUT ^= BIT0;                  // P1.0 = toggle
-        else
+            P6OUT &= ~BIT6;
+        }
+        else {
             P6OUT ^= BIT6;                 // P6.6 = toggle
+            P1OUT &= ~BIT0;
+        }
         __delay_cycles(100000);
     }
 }
@@ -80,16 +87,22 @@ __interrupt void Port_2(void)
 {
     P2IFG &= ~BIT3;                         // Clear P1.3 IFG
 
-    if ( )       // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a rising edge.
+    if (risingEdge)       // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a rising edge.
     {
         LED_Color = 0;
         // @TODO Add code to change which edge the interrupt should be looking for next
+        risingEdge = 0x00;      // Tells interrupt to look for falling edge by setting variable to 1
+        fallingEdge = 0x01;
+        P2IES &= ~BIT3;                         // P2.3 Low --> High edge
     }
 
-    else if ( ) // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a falling edge.
+    else if (fallingEdge) // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a falling edge.
     {
         LED_Color = 1;
         // @TODO Add code to change which edge the interrupt should be looking for next
+        risingEdge = 0x01;      // Tells interrupt to look for rising edge by setting variable to 1
+        fallingEdge = 0x00;
+        P2IES |= BIT3;                         // P2.3 High --> Low edge
     }
 }
 
